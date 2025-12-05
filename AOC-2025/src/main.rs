5fd40a174e9735cd5b3df1c45efb54e27e5c67a9
SherisseTJW <tanjingwensherisse@gmail.com>
2025-12-05 13:57:14 +0800

    wip: Day 5 Pt 1

--- AOC-2025/src/main.rs
+++ AOC-2025/src/main.rs
@@ -1,5 +1,12 @@
+use core::num;
 use std::env;
 use std::fs;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+struct Interval {
+    start: i64,
+    end: i64,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,75 +20,107 @@ fn main() {
     // println!("With text:\n{}", contents);
 
     // NOTE: Logic below
-    let mut grid: Vec<Vec<char>> = vec![];
-    let rows = contents.trim().split("\n");
+    let mut intervals: Vec<Interval> = vec![];
 
-    for row in rows {
-        let cur = row.chars().collect();
-        grid.push(cur);
+    for line in contents.lines() {
+        if line.is_empty() {
+            break;
+        }
+
+        let mut split = line.trim().split("-");
+        let fst = split.next().unwrap().trim().parse::<i64>().unwrap();
+        let lst = split.next().unwrap().trim().parse::<i64>().unwrap();
+
+        intervals.push(Interval { start: fst, end: lst });
     }
 
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
+    intervals.sort_by_key(|i| i.start);
 
-    let mut total = 0;
-    for i in 0..num_rows {
-        for j in 0..num_cols {
-            if grid[i][j] == '.' {
-                continue;
-            }
 
-            let mut count = 0;
-            if i > 0 {
-                // Top
-                if grid[i - 1][j] == '@' {
-                    count += 1;
+    // let mut i = 0;
+    // loop {
+    //     let num_intervals = intervals.len();
+    //     if i >= num_intervals - 1 {
+    //         break;
+    //     }
+    //
+    //     let cur_interval = intervals[i];
+    //     let nxt_interval = intervals[i + 1];
+    //
+    //     if nxt_interval.start <= cur_interval.end {
+    //         intervals[i].end = nxt_interval.end;
+    //         intervals.remove(i + 1);
+    //
+    //         if i > 0 {
+    //             i -= 1;
+    //         }
+    //     }
+    //     else {
+    //         i += 1;
+    //     }
+    // }
+
+    // for interval in intervals {
+    //     println!("Interval: {} to {}", interval.start, interval.end);
+    // }
+
+    // NOTE: IDK HOW TO START FROM THE MIDDLE LMAO
+    let mut fresh_count = 0;
+
+    let mut flag = false;
+    for line in contents.lines() {
+        if line.is_empty() {
+            flag = true;
+            continue;
+        }
+
+        if flag {
+            let cur = line.trim().parse::<i64>().unwrap();
+
+            for interval in &intervals {
+                if cur > interval.end {
+                    continue;
                 }
 
-                // Top left
-                if j > 0 && grid[i - 1][j - 1] == '@' {
-                    count += 1;
+                if cur <= interval.end && cur >= interval.start {
+                    fresh_count += 1;
                 }
-
-                // Top right
-                if j < num_cols - 1 && grid[i - 1][j + 1] == '@' {
-                    count += 1;
-                } 
-            }
-
-            if i < num_rows - 1 {
-                // Bottom
-                if grid[i + 1][j] == '@' {
-                    count += 1;
-                }
-
-                // Bottom left
-                if j > 0 && grid[i + 1][j - 1] == '@' {
-                    count += 1;
-                }
-
-                // Bottom right
-                if j < num_cols - 1 && grid[i + 1][j + 1] == '@' {
-                    count += 1;
-                } 
-            }
-
-            // Left
-            if j > 0 && grid[i][j - 1] == '@' {
-                count += 1;
-            }
-
-            // Right
-            if j < num_cols - 1 && grid[i][j + 1] == '@' {
-                count += 1;
-            }
-
-            if count < 4 {
-                // println!("Found at ({}, {})", i, j);
-                total += 1;
             }
         }
     }
 
-    println!("Total: {}", total);
+    println!("Fresh count: {}", fresh_count);
+
+    // let mut fresh_ids: HashSet<i64> = HashSet::new();
+    // let mut flag = false;
+    //
+    // let mut fresh_count = 0;
+    // for line in contents.lines() {
+    //     if line.is_empty() {
+    //         flag = true;
+    //         continue;
+    //     }
+    //
+    //     // Checking if fresh
+    //     if flag {
+    //         let cur = line.trim().parse::<i64>().unwrap();
+    //
+    //         if fresh_ids.contains(&cur) {
+    //             fresh_count += 1;
+    //         }
+    //     }
+    //
+    //     // Still reading fresh id ranges
+    //     else {
+    //         let mut split = line.trim().split("-");
+    //         let fst = split.next().unwrap().trim().parse::<i64>().unwrap();
+    //         let lst = split.next().unwrap().trim().parse::<i64>().unwrap();
+    //
+    //         for i in fst..=lst {
+    //             fresh_ids.insert(i);
+    //         }
+    //     }
+    // }
+    //
+    // println!("Fresh Count: {}", fresh_count);
 }
